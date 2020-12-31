9f986b53b75840014121cd4c03b3c5921efac923
Chetanajs <76551002+Chetanajs@users.noreply.github.com>
2021-01-01 00:08:08 +0530

    final

--- myApp.js
+++ myApp.js
@@ -3,10 +3,37 @@ var textinput=document.querySelector("#text-in")
 var outdiv=document.querySelector("#box")
 console.log(textinput)
 
+var serverURL= "https://api.funtranslations.com/translate/minion.json"
+
+
+
+function gettransURL(input){
+    return serverURL +"?" +"text"+input
+}
+function errorhandler(error){
+    console.log("errorboccured",error);
+    alert("something wrong with server! try again")
+}
+
+function clickhandler(){
+    var inouttext=inputtext.value;//taking input
+}
+
+//calling server for processing
+
+fetch (gettransURl(inputtext))
+.then(Response=>Response.json())
+.then (json=>{
+    var transtext=json.contents.translated;
+    outdiv.innertext=transtext;
+    )}
+.catch(errorhandler)
 
 function clickeventhandler(){
     console.log("translated")
     console.log("input",textinput.value)
 };
+
+
 
 buttontrans.addEventListener("click",clickeventhandler)
