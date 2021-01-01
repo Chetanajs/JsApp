2a7e3da1d4179381718ce00e5ec2ab14131ece36
Chetanajs <76551002+Chetanajs@users.noreply.github.com>
2021-01-01 14:13:50 +0530

    corrected

--- myApp.js
+++ myApp.js
@@ -1,39 +1,33 @@
 var buttontrans = document.querySelector("#bt-trans")
 var textinput=document.querySelector("#text-in")
-var outdiv=document.querySelector("#box")
+var outdiv=document.querySelector("#outputdiv")
 console.log(textinput)
 
 var serverURL= "https://api.funtranslations.com/translate/minion.json"
 
 
 
-function gettransURL(input){
-    return serverURL +"?" +"text"+input
+function gettransURL(text){
+    return serverURL +"?" +"text"+ text
 }
+
 function errorhandler(error){
     console.log("errorboccured",error);
     alert("something wrong with server! try again")
 }
 
 function clickhandler(){
-    var inouttext=inputtext.value;//taking input
-}
+    var inputtext=textinput.value;//taking input
 
 //calling server for processing
 
 fetch (gettransURl(inputtext))
-.then(Response=>Response.json())
-.then (json=>{
-    var transtext=json.contents.translated;
-    outdiv.innertext=transtext;
-    )}
+    .then(Response=>Response.json())
+    .then (json=>{
+        var transtext=json.contents.translated;
+        outdiv.innertext=transtext;
+    })
 .catch(errorhandler)
-
-function clickeventhandler(){
-    console.log("translated")
-    console.log("input",textinput.value)
 };
-
-
 
 buttontrans.addEventListener("click",clickeventhandler)
